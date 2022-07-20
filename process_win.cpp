5574ced050bc32fa33d145628f7806b86265673b
Alexander Bock <alexander.bock@liu.se>
2022-07-20 14:44:46 +0200

    Never inherit file descriptors

--- process_win.cpp
+++ process_win.cpp
@@ -135,7 +135,7 @@ Process::id_type Process::open(const string_type &command, const string_type &pa
 #endif
   }
   BOOL bSuccess = CreateProcess(nullptr, process_command.empty() ? nullptr : &process_command[0], nullptr, nullptr,
-                                stdin_fd || stdout_fd || stderr_fd || config.inherit_file_descriptors, // Cannot be false when stdout, stderr or stdin is used
+                                FALSE,
                                 stdin_fd || stdout_fd || stderr_fd ? CREATE_NO_WINDOW : 0,             // CREATE_NO_WINDOW cannot be used when stdout or stderr is redirected to parent process
                                 environment_str.empty() ? nullptr : &environment_str[0],
                                 path.empty() ? nullptr : path.c_str(),
